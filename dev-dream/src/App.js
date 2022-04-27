be1b5a603cb7e6cdc535d6d1f923d6324c2e1190
Alejandro Benitez <alejandrobenitezcorp@outlook.com>
2022-04-27 20:48:45 -0300

    Adding react router dom, creating components and config the basic navigation

--- dev-dream/src/App.js
+++ dev-dream/src/App.js
@@ -1,7 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="App">
+      <h1>Welcome to DevDream!</h1>
+      <div className="bg-slate-300 h-screen text-white flex">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </div>
     </div>
   );
 };
